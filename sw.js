f2a06857c97ff017bb69d5a0f8ff3c2c85d8cf72
William Powell "wmpowell8 <88069370+wmpowell8@users.noreply.github.com>
2022-04-26 21:32:20 -0700

    Added everything needed for PWA (besides start-url, which doesn't work for some reason) (also PWA = Progressive Web App)

---
+++ sw.js
@@ -0,0 +1,35 @@
+// importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+  console.log("%cInstalling Progressive Web App (PWA)", "color: purple;");
+  e.waitUntil(
+    caches.open('p5js-tetrominoes').then(function(cache) {
+      return cache.addAll([
+        "./icon.png",
+        "./index.html",
+        "./LICENSE.txt",
+        "./manifest.json",
+        "./README.md",
+        "./showdown.js",
+        "./sw.js",
+        "./p5js-tetrominoes/game.html",
+        "./p5js-tetrominoes/assets/Roboto-Regular.ttf",
+        "./p5js-tetrominoes/lib/p5.min.js",
+        "./p5js-tetrominoes/lib/scenemanager.js",
+        "./p5js-tetrominoes/src/GameManager.js",
+        "./p5js-tetrominoes/src/GameOver.js",
+        "./p5js-tetrominoes/src/Matrix.js",
+        "./p5js-tetrominoes/src/Menu.js",
+        "./p5js-tetrominoes/src/PositionedTetromino.js",
+        "./p5js-tetrominoes/src/RS.js",
+        "./p5js-tetrominoes/src/ScoringMessage.js",
+        "./p5js-tetrominoes/src/sketch.js",
+        "./p5js-tetrominoes/src/SRS.js",
+        "./p5js-tetrominoes/src/Tetromino.js",
+        "./p5js-tetrominoes/src/WinCondition.js"
+      ]);
+    })
+  );
+  console.log("%cProgressive Web App (PWA) Successfully Installed", "color: green;");
+});
